85a650a2d6dd6fe496a28f5c170ebe99d7a978a6
Akababa <michael3795@gmail.com>
2017-10-08 17:02:46 -0500

    thing

--- client/dist/js/main.js
+++ client/dist/js/main.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 /*
 	FlatApp Script
 */
@@ -120,8 +122,26 @@
     /*=========================================================================
     Subscribe Form
 =========================================================================*/
+function loadJSON(file, callback) {   
 
-    var awslambdagateway = "https://rhkuew0ne5.execute-api.us-east-1.amazonaws.com/dev";
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', file, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+ 
+    
+    loadJSON("data.json", function(response) {
+  
+        var awslambdagateway = JSON.parse(response).ServiceEndpoint;
+        console.log(actual_JSON);
+    });
 
     function toObject(names, values) {
         var result = {};
