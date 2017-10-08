f5460a416f38b3a649d5f9813922c62f9aea8852
Akababa <michael3795@gmail.com>
2017-10-07 20:45:57 -0500

    removed stuff

--- api/connect.bat
+++
@@ -1,1 +0,0 @@
-lambda-local -l app.js -e event.json -E '{\"MONGODB_ATLAS_CLUSTER_URI\":\"mongodb://lambdauser:lambdapass@cluster0-shard-00-00-wwwhr.mongodb.net:27017,cluster0-shard-00-01-wwwhr.mongodb.net:27017,cluster0-shard-00-02-wwwhr.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin\"}'
