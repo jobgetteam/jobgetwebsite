acdcf30ae9300f48f09db82e86e86135aba8ce21
Akababa <michael3795@gmail.com>
2017-10-11 12:22:47 -0500

    it works

--- client/dist/js/main.js
+++ client/dist/js/main.js
@@ -141,12 +141,49 @@ function loadJSON(file, callback) {
         console.log(awslambdagateway);
     });
 
+// This example displays an address form, using the autocomplete feature
+// of the Google Places API to help users fill in the information.
+
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
+var autocomplete = new google.maps.places.Autocomplete(
+      /** @type {!HTMLInputElement} */(document.getElementById('auto-address')),
+      {types: ['geocode']});
+
+  // When the user selects an address from the dropdown, populate the address
+  // fields in the form.
+  //autocomplete.addListener('place_changed', fillInAddress);
+
+
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy
+      });
+      autocomplete.setBounds(circle.getBounds());
+    });
+  }
+}
+
     function toObject(names, values) {
         var result = {};
         for (var i = 0; i < names.length; i++)
             result[names[i]] = values[i];
         return result;
     }
+    $('#auto-address').focus(geolocate)
 
     $('.comp-form').find("fieldset").hide();
     $('.comp-btn').on('click', function(event) {
